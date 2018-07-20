7815cb53ad54feee740c354704d5ccf8e23738a4
Robert Wallis <smilingrob@gmail.com>
2018-07-19 23:21:40 -0700

    C++ trigger events

--- Source/Shippy/PlatformTrigger.h
+++ Source/Shippy/PlatformTrigger.h
@@ -30,6 +30,9 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
-	
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
